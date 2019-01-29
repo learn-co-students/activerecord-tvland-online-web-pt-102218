88f362d2dda6e884bf391184ac2e7488cdde6ab4
Linda Chang <lindachang186@gmail.com>
2019-01-29 17:45:31 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,5 +8,10 @@ class Actor < ActiveRecord::Base
     "#{self.first_name} #{self.last_name}"
   end
 
+  def list_roles
+    self.characters.map do |character|
+      "#{character.name} - #{character.show.name}"
+    end
+  end
 
 end
