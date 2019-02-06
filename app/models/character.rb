93efac81b5cb69c6db7fea277c32e421dd493934
Azraf Chowdhury <achowdhur@ltu.edu>
2019-02-06 20:31:54 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,14 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+	belongs_to :show
+  belongs_to :network
+
+  def say_that_thing_you_say
+   "#{self.name} always says: #{self.catchphrase}"
+ end
+
+ def build_show(show_name)
+   self.show = Show.new(show_name)
+ end
+
 end
